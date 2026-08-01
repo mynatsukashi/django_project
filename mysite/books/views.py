033c5798df9f03f2f975851bbe0b03d896b3b140
Natali Nasidze <natali.nasidze.1@gmail.com>
2026-08-01 23:15:47 +0400

    push on Github. Create_post

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
-from books.models import Review
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from books.models import Author, Book, Genre, Review
 
+@login_required
 def home_view(request):
+    if request.method == "POST":
+       post = Review.objects.create(
+            user = request.user,
+            book=request.POST.get("book"),
+            author_name = request.POST.get("author_name"),
+            author_surname = request.POST.get("author_surname"),
+            genres = request.POST.get("genres"),
+            rating = request.POST.get("rating"),
+            content = request.POST.get("content")
+        )
     return render(
         request,
         "home.html",
